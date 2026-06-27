46e8a56184fcba27279b365d950702c8852eefad
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2026-06-26 23:44:16 -0300

    Implementar padrão AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,25 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/');
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-  //Checkpoint 1: Verificar se a página de landing está carregada
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
-  //Checkpoint 2: Navegar para a página de consulta de pedidos
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-
-  //Checkpoint 3: Preencher o campo de busca com o número do pedido
+  //Act
   await page.getByTestId('search-order-id').click();
-  await page.getByTestId('search-order-id').fill('VLO-3EGJD7');
+  await page.getByTestId('search-order-id').fill('VLO-3EGJD7')
+  await page.getByTestId('search-order-button').click()
 
-  //Checkpoint 4: Clicar no botão de busca
-  await page.getByTestId('search-order-button').click();
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-3EGJD7');
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-3EGJD7')
 
-  //Checkpoint 5: Verificar se o pedido foi encontrado e está aprovado
   await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  await expect(page.getByTestId('order-result-status')).toContainText( 'APROVADO')
 });
