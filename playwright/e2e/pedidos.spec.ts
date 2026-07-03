787a6fbc91c5627354e20e2f78b488d26783c13e
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2026-07-02 22:43:03 -0300

    Code Challenge: Sem usar nenhum test-id

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,26 +1,30 @@
-import { test, expect } from "@playwright/test"
+import { test, expect } from "@playwright/test";
 
 /// AAA - Arrange, Act, Assert
 
 test("deve consultar um pedido aprovado", async ({ page }) => {
   //Arrange
-  await page.goto("http://localhost:5173/")
+  await page.goto("http://localhost:5173/");
   await expect(
     page.getByTestId("hero-section").getByRole("heading"),
-  ).toContainText("Velô Sprint")
+  ).toContainText("Velô Sprint");
 
-  await page.getByRole("link", { name: "Consultar Pedido" }).click()
-  await expect(page.getByRole("heading")).toContainText("Consultar Pedido")
+  await page.getByRole("link", { name: "Consultar Pedido" }).click();
+  await expect(page.getByRole("heading")).toContainText("Consultar Pedido");
 
   //Act
-  await page.getByTestId("search-order-id").fill("VLO-3EGJD7")
-  await page.getByRole("button", { name: "Buscar Pedido" }).click()
+  await page.getByTestId("search-order-id").fill("VLO-3EGJD7");
+  await page.getByRole("button", { name: "Buscar Pedido" }).click();
 
   //Assert
-  const orderResult = page.getByTestId("order-result-VLO-3EGJD7") //Busca apenas pela DIV e resultado, evitando pegar o valor fora dessa DIV e serve para ambas as validações necessárias
+  const orderCode = "VLO-3EGJD7"
+  const expectedStatus = "APROVADO"
+  const orderInfo = page.getByText(orderCode, { exact: true }).locator("..")
 
-  await expect(orderResult).toBeVisible({ timeout: 10000 })
-  await expect(orderResult.getByText("VLO-3EGJD7")).toBeVisible()
+  await expect(orderInfo).toContainText("Pedido")
+  await expect(orderInfo).toContainText(orderCode)
 
-  await expect(orderResult.getByText("APROVADO")).toBeVisible()
+  const orderHeader = orderInfo.locator("..").locator("..")
+
+  await expect(orderHeader).toContainText(expectedStatus)
 });
