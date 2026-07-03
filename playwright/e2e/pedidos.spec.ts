94dd2b1814f611fa94ee0ef8f9aad470247c6814
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2026-07-02 22:01:50 -0300

    desafio Code Challenge: Você consegue testar sem Test-ID

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,23 +1,26 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test"
 
 /// AAA - Arrange, Act, Assert
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
+test("deve consultar um pedido aprovado", async ({ page }) => {
   //Arrange
-  await page.goto('http://localhost:5173/');
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  await page.goto("http://localhost:5173/")
+  await expect(
+    page.getByTestId("hero-section").getByRole("heading"),
+  ).toContainText("Velô Sprint")
 
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+  await page.getByRole("link", { name: "Consultar Pedido" }).click()
+  await expect(page.getByRole("heading")).toContainText("Consultar Pedido")
 
   //Act
-  await page.getByTestId('search-order-id').fill('VLO-3EGJD7')
-  await page.getByTestId('search-order-button').click()
+  await page.getByTestId("search-order-id").fill("VLO-3EGJD7")
+  await page.getByRole("button", { name: "Buscar Pedido" }).click()
 
   //Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10000});
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-3EGJD7')
+  const orderResult = page.getByTestId("order-result-VLO-3EGJD7") //Busca apenas pela DIV e resultado, evitando pegar o valor fora dessa DIV e serve para ambas as validações necessárias
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText( 'APROVADO')
+  await expect(orderResult).toBeVisible({ timeout: 10000 })
+  await expect(orderResult.getByText("VLO-3EGJD7")).toBeVisible()
+
+  await expect(orderResult.getByText("APROVADO")).toBeVisible()
 });
