5e9b68fa6c3ff15f1280387ea5cdaa777c9d981a
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2026-07-05 22:08:08 -0300

    Ajuste do teste após correção do desafio

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,30 +1,28 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test'
 
 /// AAA - Arrange, Act, Assert
 
-test("deve consultar um pedido aprovado", async ({ page }) => {
+test('deve consultar um pedido aprovado', async ({ page }) => {
   //Arrange
-  await page.goto("http://localhost:5173/");
+  await page.goto('http://localhost:5173/')
   await expect(
-    page.getByTestId("hero-section").getByRole("heading"),
-  ).toContainText("Velô Sprint");
+    page.getByTestId('hero-section').getByRole('heading'),
+  ).toContainText('Velô Sprint')
 
-  await page.getByRole("link", { name: "Consultar Pedido" }).click();
-  await expect(page.getByRole("heading")).toContainText("Consultar Pedido");
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   //Act
-  await page.getByTestId("search-order-id").fill("VLO-3EGJD7");
-  await page.getByRole("button", { name: "Buscar Pedido" }).click();
+  await page.getByTestId('search-order-id').fill('VLO-3EGJD7')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   //Assert
-  const orderCode = "VLO-3EGJD7"
-  const expectedStatus = "APROVADO"
-  const orderInfo = page.getByText(orderCode, { exact: true }).locator("..")
+  const containerPedido = page
+    .getByRole('paragraph')
+    .filter({ hasText: /^Pedido$/ })
+    .locator('..') // Sobe para o elemento pai (a div que agrupa ambos)
 
-  await expect(orderInfo).toContainText("Pedido")
-  await expect(orderInfo).toContainText(orderCode)
+  await expect(containerPedido).toContainText('VLO-3EGJD7', { timeout: 10_000 })
 
-  const orderHeader = orderInfo.locator("..").locator("..")
-
-  await expect(orderHeader).toContainText(expectedStatus)
-});
+  await expect(page.getByText('APROVADO')).toBeVisible()
+})
