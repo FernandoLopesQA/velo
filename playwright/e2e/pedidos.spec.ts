7f9f460a9275e179216041243ed664c50349ac0b
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2026-07-08 23:00:12 -0300

    aula 1 - módulo 3

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -112,6 +112,55 @@ test.describe('Consulta de pedido', () => {
     `)
   })
 
+    test('deve consultar um pedido em análise', async ({ page }) => {
+    // Test Data
+    const order = {
+      number: 'VLO-KOFFJ2',
+      status: 'EM_ANALISE',
+      color: 'Lunar whithe',
+      wheels: 'Aero Wheels',
+      customer: {
+        name: 'Logan Wolverine',
+        email: 'carcaju@xmen.com',
+      },
+      payment: 'À Vista',
+    }
+
+    //Act
+    await page.getByTestId('search-order-id').fill(order.number)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+    //Assert
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+    - img
+    - paragraph: Pedido
+    - paragraph: ${order.number}
+    - img
+    - text: ${order.status}
+    - img "Velô Sprint"
+    - paragraph: Modelo
+    - paragraph: Velô Sprint
+    - paragraph: Cor
+    - paragraph: ${order.color}
+    - paragraph: Interior
+    - paragraph: cream
+    - paragraph: Rodas
+    - paragraph: ${order.wheels}
+    - heading "Dados do Cliente" [level=4]
+    - paragraph: Nome
+    - paragraph: ${order.customer.name}
+    - paragraph: Email
+    - paragraph: ${order.customer.email}
+    - paragraph: Loja de Retirada
+    - paragraph
+    - paragraph: Data do Pedido
+    - paragraph: /\\d+\\/\\d+\\/\\d+/
+    - heading "Pagamento" [level=4]
+    - paragraph: ${order.payment}
+    - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+    `)
+  })
+
   test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
     // Test Data
     const order = generateOrderCode()
